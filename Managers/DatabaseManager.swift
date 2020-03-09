a44a133ea33aa98af9d7e4efb644630a273a1dd8
Vladimir Vasilyev <v.vasilyev@ivideon.com>
2020-03-09 21:40:26 +0100

    parsing json into models

--- Managers/DatabaseManager.swift
+++ Managers/DatabaseManager.swift
@@ -14,6 +14,8 @@ class DatabaseManager: NSObject {
     private var experienceWorks: [WorkExperienceModel] = []
     private var skills: [SkillModel] = []
     
+    
+    //TODO: move into build settings user defined
     private let urlString = "https://gist.githubusercontent.com/Snakendead/9ce9cc098d06126c5491ddef2d2530fe/raw/42678529ceff99db90441fcefdf84e27204d1de5/gistfile1.txt"
     
     private var successCallback: (() -> Void)? = nil
@@ -75,7 +77,86 @@ class DatabaseManager: NSObject {
     }
     
     private func parseResult(dict: [String: AnyObject]) {
-        print(dict)
+        
+        if let infoObject = dict["info"] as? [String: AnyObject] {
+            parseInfo(dict: infoObject)
+        }
+        
+        if let experienceObject = dict["experience"] as? [String: AnyObject] {
+            let array = parseArray(with: "e", dict: experienceObject)
+            parseExperienceArray(dictArray: array)
+        }
+        
+        if let skillsObject = dict["skills"] as? [String: AnyObject] {
+            let array = parseArray(with: "s", dict: skillsObject)
+            parseSkillsArray(dictArray: array)
+        }
+        
+        successCallback?()
+    }
+    
+    private func parseArray(with prefix: String, dict: [String: AnyObject]) -> [[String:AnyObject]] {
+        var result = [[String:AnyObject]]()
+
+        var increment = 1
+        while let object = dict["\(prefix)\(increment)"] as? [String: AnyObject] {
+            result.append(object)
+            increment += 1
+        }
+        
+        return result
+    }
+    
+    //MARK: parsing objects
+    
+    private func parseInfo(dict: [String: AnyObject]) {
+        guard let name = dict["name"] as? String,
+            let surname = dict["surname"] as? String,
+            let phone = dict["phone"] as? String,
+            let lat = dict["locationLat"] as? Double,
+            let lon = dict["locationLon"] as? Double else {
+                return
+        }
+        
+        let info = InfoModel(with: name,
+                             surname: surname,
+                             phone: phone,
+                             locationLatitude: lat,
+                             locationLongitude: lon)
+        
+        self.infoModel = info
+    }
+    
+    private func parseExperienceArray(dictArray: [[String: AnyObject]]) {
+        for dict in dictArray {
+            guard let role = dict["role"] as? String,
+                let start = dict["start"] as? String,
+                let end = dict["end"] as? String,
+                let company = dict["company"] as? String,
+                let info = dict["description"] as? String else {
+                    return
+            }
+
+            let object = WorkExperienceModel(with: company,
+                                             role: role,
+                                             info: info,
+                                             startTime: start,
+                                             endTime: end)
+            experienceWorks.append(object)
+        }
+    }
+    
+    private func parseSkillsArray(dictArray: [[String: AnyObject]]) {
+        for dict in dictArray {
+            guard let name = dict["name"] as? String,
+                let grade = dict["grade"] as? Int,
+                let info = dict["description"] as? String else {
+                    return
+            }
+
+            let object = SkillModel(with: name, grade: grade, info: info)
+            skills.append(object)
+        }
     }
 
 }
